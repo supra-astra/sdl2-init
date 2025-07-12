00d2986f716fd4e5cf4a4903bf955a8836a24307
Sagnik Chatterjee <sagnikchatterjee607@gmail.com>
2025-07-12 16:03:53 +0530

    feat: change from cpp to c

--- src/main.c
+++ src/main.c
@@ -1,19 +1,19 @@
 #include <SDL.h>
-#include <iostream>
+#include <stdio.h>
 
 int main() {
-    std::cout << "Hello, SDL!" << std::endl;
+    printf("Hello, SDL!\n");
 
     // Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        fprintf(stderr,"SDL_Init Error: %s\n",SDL_GetError());
         return 1;
     }
 
     // Create a window
     SDL_Window* window = SDL_CreateWindow("SDL Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
     if (window == nullptr) {
-        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        fprintf( stderr,"SDL_CreateWindow Error: %s\n",SDL_GetError());
         SDL_Quit();
         return 1;
     }
@@ -21,7 +21,7 @@ int main() {
     // Create a renderer
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if (renderer == nullptr) {
-        std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+        fprintf(stderr,"SDL_CreateRenderer Error: %s\n",SDL_GetError());
         SDL_DestroyWindow(window);
         SDL_Quit();
         return 1;
